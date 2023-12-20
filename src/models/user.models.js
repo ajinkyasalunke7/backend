bef24b92ef96be25345fc62e7622921012363651
ajinkyasalunke7 <ajinkyasalunke90@outlook.com>
2023-12-20 09:35:36 +0530

    bug fixed

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -66,7 +66,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = async function () {
+userSchema.methods.generateAccessToken =  function () {
     return jwt.sign({
         _id: this._id,
         email: this.email,
@@ -80,7 +80,7 @@ userSchema.methods.generateAccessToken = async function () {
     )
 }
 
-userSchema.methods.generateRefreshToken = async function () {
+userSchema.methods.generateRefreshToken =  function () {
     return jwt.sign({
         _id: this._id,
     },
